3beb0fae162bf7d017d138739f80914b0b1fcd31
moshonk <omondi.ken@gmail.com>
2023-05-05 07:57:10 +0300

    made the deploy scrit generic by converting hard coded values to env variables

--- bemrs-deploy/deploy.sh
+++ bemrs-deploy/deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash 
 cd ~/Development/bitbucket.com/openmrs-module-botswanaemr
 mvn clean install -DskipTests
-echo "Copying data to server"
+echo "Copying botswanaemr module to server $SERVER" 
 scp omod/target/botswanaemr*.omod $USER@$SERVER:/opt/docker-emr/web/modules
 cd ~/openmrs/refapp250/
 zip -r configuration.zip configuration
-echo "Copying configuration data to server"
+echo "Copying configuration data to server $SERVER" 
 scp configuration.zip $USER@$SERVER:/opt/docker-emr/web/
-echo "logging in to server"
+echo "logging in to server $SERVER to restart services" 
 ssh $USER@$SERVER "cd /opt/docker-emr/web/ && unzip -o configuration.zip | docker restart bemrs_web"
 echo "logging in to server"
 
